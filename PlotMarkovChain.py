f35b5bdfe01314bc9f23324da81e09737aadb768
Vicente Garza Reyna <vicente.garzara@uanl.edu.mx>
2021-05-05 18:28:53 -0500

    Separated simulation and plot into different files

---
+++ PlotMarkovChain.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import networkx as nx
+import mathplotlib.pyplot as plt
+
+probs = np.loadtxt("matrix.csv", delimiter=',')
+max_points = len(probs())
+
+G = nx.Graph()
+
+for i in range(max_points):
+    for j in range(max_points):
+        start_node, end_node = i+1, j+1
+        if start_node == 1:
+            start_node = 0
+        if end_node == 1:
+            end_node = 0
+        if probs[i][j] > 0:
+            G.add_edge( start_node, end_node, weight=probs[i][j] )
+
+
+pos = nx.spiral_layout(G)  # positions for all nodes
+
+plt.figure(1, figsize=(48*4,48*4))
+
+# nodes
+nx.draw_networkx_nodes(G, pos, node_color=range(max_points), node_size=800*16)
+
+# edges
+edge_number = len(G.edges)
+nx.draw_networkx_edges(G, pos,
+                       width=3*4,
+                       arrows=True,
+                       arrowstyle="->",
+                       #arrowsize = 100,
+                       alpha=(0.5),)
+
+# labels
+edge_labels = nx.get_edge_attributes(G, "weight")
+edge_labels = {each: int(1000*edge_labels[each])/1000 for each in nx.get_edge_attributes(G, "weight")}
+
+nx.draw_networkx_labels(G, pos, font_size=20*4, font_family="sans-serif")
+nx.draw_networkx_edge_labels(G, pos,
+                             font_size=20*2, 
+                             font_family="sans-serif", 
+                             edge_labels=edge_labels, 
+                             label_pos=0.1, )
+
+plt.axis("off")
+plt.show()
--- SimulatePig.py
+++ SimulatePig.py
@@ -5,13 +5,14 @@ import random
 
 np.set_printoptions(precision=3, suppress=True, linewidth=200)
 
+
 # Configurar parámetros
-throws = 5_000_000
+throws = 50_000_000
 max_points = 100
 steps = 50
 
 # Inicializar cuenta
-count = [ [ 0 for _ in range(max_points-1) ] for _ in range(max_points-1) ]
+count = [ [ 0 for _ in range(max_points) ] for _ in range(max_points) ]
 
 # Contar trancisiones
 points = 0
@@ -21,12 +22,14 @@ for _ in range( throws ):
     if dice_roll == 1:
         count[row][0] += 1
     elif points + dice_roll >= max_points:
+        count[row][max_points-1] += 1
+        count[max_points-1][0] += 1
         points = 0
     else:
         count[row][points + dice_roll-1] += 1
         points = points + dice_roll
 
-# Estimar probabilidades
+# Estimar probabilidades basado en las cuentas
 probs = []
 for each in count:
     row = []
@@ -36,21 +39,7 @@ for each in count:
         except:
             row.append( 0 )
     probs.append(row)
-probs = np.array( probs )
 
-# Solo puedes empezar con 0 puntos
-initial_probs = np.array([ 0 for _ in range(max_points-1) ])
-initial_probs[0] = 1
+probs = np.asarray( probs )
+np.savetxt("matrix.csv", probs, delimiter=',')
 
-# Buscar cantidad de lanzamientos que maximizen las
-# probabilidades de llegar a 100 puntos
-max_prob = -1
-max_prob_pos = 0
-result = probs
-for i in range(steps):
-    result = result @ probs
-    if (initial_probs@result)[-1] > max_prob:
-        max_prob = (initial_probs@result)[-1]
-        max_prob_pos = i+2
-    
-print(max_prob_pos, ".-", max_prob)
