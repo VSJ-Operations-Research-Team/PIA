9784f6355c67b7b1416e96c100102332c27481ba
JARED EVANDER FUENTES SANCHEZ <jared.fuentessz@uanl.edu.mx>
2021-05-06 18:14:08 -0500

    Raising matrix to the 10th power

--- Transition_Matrix.py
+++ Transition_Matrix.py
@@ -10,6 +10,10 @@ for i in range(max_points + 1):
         continue
     indice.append(i)
 
+power = np.linalg.matrix_power(probs, 10)
+power = np.asarray( power )
+np.savetxt("matrix_power_10.csv", power, delimiter=',')
+
 for i in range(max_points):
     for j in range(max_points):
         probs[i][j] = probs[i][j]*100
